69bb9f21407bd5293153181366da5bde173764de
Michael Trumpf <miketrumpf@gmail.com>
2015-05-07 13:30:32 -0400

    added application controller

--- texteml8r_app/config/routes.rb
+++ texteml8r_app/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  
+  root to: "application#index"
+
+  resources :users
+  resources :texts
+  resources :contacts
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
