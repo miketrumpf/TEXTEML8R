04a9e363c9170dec2458a5b80e404e88ff9482b1
Michael Trumpf <miketrumpf@gmail.com>
2015-05-13 12:05:43 -0400

    fixed sass heroku issue 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       respond_to do |format|
-        format.html {redirect_to users_path}
+        format.html {redirect_to sessions_new_path}
         format.json {render json: @user}
       end
     else
